370a3cc8a52437cdb55a899f0efbd5a22e0fc2de
svmihar <svmihar@gmail.com>
2019-12-11 23:31:39 +0700

    :white_check_mark: done day10

--- day10/day10.py
+++ day10/day10.py
@@ -1,5 +1,6 @@
 import math
-from typing import List, Dict, NamedTuple, Tuple
+from collections import defaultdict
+from typing import List, Dict, NamedTuple, Tuple, Iterator
 
 
 class Asteroid(NamedTuple):
@@ -90,3 +91,58 @@ assert best_station(test_3) == Asteroid((11, 13), 210)
 
 input_ = open('input.txt').read()
 print(best_station(parse(input_)))
+
+
+def faux_angle(asteroid): 
+    dx,dy = asteroid 
+    if dx == 0 and dy < 0: 
+        return (0,0)
+    elif dx > 0 and dy<0: 
+        return (1,dx/abs(dy))
+    elif dx > 0 and dy == 0: 
+        return (2,0)
+    elif dx > 0 and dy > 0: 
+        return (3, dy/dx) 
+    elif dx == 0 and dy > 0: 
+        return (4,0)
+    elif dx < 0 and dy > 0: 
+        return (5, abs(dx)/dy)
+    elif dx < 0  and dy == 0: 
+        return (6,0)
+    elif dx < 0 and dy < 0: 
+        return (7, dy/dx)
+
+def iterate(asteroids: Asteroids, station: Asteroid) -> Iterator[Asteroid]: 
+    asteroids_by_angle = defaultdict(list)
+
+    for x, y in asteroids: 
+        dx = x - station.x 
+        dy = y - station.y
+
+        fpb = math.gcd(dx, dy)
+
+        if dx == dy == 0:
+            pass
+        else:
+            angle = (dx / fpb, dy / fpb)
+            asteroids_by_angle[angle].append(Asteroid(x,y))
+    # sort length descending for each angle
+    for angle_asteroids in asteroids_by_angle.values(): 
+        angle_asteroids.sort(key=lambda a: abs(a.x - station.x) + abs(a.y - station.y), reverse= True)
+
+    while asteroids_by_angle: 
+        keys = asteroids_by_angle.keys() # dx, dy nya 
+        keys = sorted(keys, key = faux_angle)
+
+        for key in keys: 
+            angle_asteroids = asteroids_by_angle[key] 
+            yield angle_asteroids.pop()
+            if not angle_asteroids: 
+                del asteroids_by_angle[key]
+
+
+
+station = best_station(parse(input_))[0]
+vaporizations = list(iterate(parse(input_), station))
+print(vaporizations[199])
+print(vaporizations[199].x * 100 +  vaporizations[199].y)
